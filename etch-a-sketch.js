3daf8ebd7c92078c388c04a467758c5baa04a55b
KallamSamad <kallamsamad@gmail.com>
2025-08-11 15:03:54 +0100

    added a for loop to make a 16x16 grid but the container has a style.display grid property

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,19 @@
 const container=document.createElement("div")
 container.style.backgroundColor = "red"
 container.textContent ="Dummy text"
+container.style.display = "grid";
+container.style.gridTemplateColumns = "repeat(16, 20px)";
+container.style.gridTemplateRows = "repeat(16, 20px)";
+container.style.gap = "1px"; 
 document.body.appendChild(container)
+
+
+
+for (let gridNum=0;  gridNum <=256; gridNum++){
+    const gridElement = document.createElement("div")
+    gridElement.style.height = "16px"
+    gridElement.style.width = "16px"
+    gridElement.style.border ="2px solid black"
+    gridElement.textContent = "a"
+    container.appendChild(gridElement)
+}
